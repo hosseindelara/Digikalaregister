c733e8517979f73a01edde3d6b49bda3b4254a37
hosseinDelara <59303167+hosseindelara@users.noreply.github.com>
2020-06-01 17:48:22 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿document.getElementById('registe').addEventListener('click', function () {
+    const mobile_input = document.getElementById('mobile-login').value;
+    const pattenr = new RegExp(/^(0|0098|\+98)9\d{9}$/);
+    const password_input = document.getElementById('kalameobor-login');
+    const pattenr_password = new RegExp(/^[a-z Z-a 0-9].+$/);
+    if (!pattenr.test(mobile_input) || !pattenr_password.test(password_input)) {
+        
+        return
+    } else {
+        return Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'فرم ثبت شد',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+});
